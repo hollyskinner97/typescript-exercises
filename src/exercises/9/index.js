dedd063563b201089aa2e5473425e159fb0a4c47
Holly Skinner <hollyskinner97@gmail.com>
2025-04-01 20:29:52 +0100

    add solution to task 9

--- src/exercises/9/index.js
+++ src/exercises/9/index.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 
 Intro:
@@ -26,121 +27,97 @@ Exercise:
     response formats for each of the functions.
 
 */
-
-interface User {
-    type: 'user';
-    name: string;
-    age: number;
-    occupation: string;
-}
-
-interface Admin {
-    type: 'admin';
-    name: string;
-    age: number;
-    role: string;
-}
-
-type Person = User | Admin;
-
-const admins: Admin[] = [
-    {type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator'},
-    {type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver'}
+exports.__esModule = true;
+exports.requestCoffeeMachineQueueLength = exports.requestCurrentServerTime = exports.requestUsers = exports.requestAdmins = void 0;
+var admins = [
+    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
+    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' }
 ];
-
-const users: User[] = [
-    {type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep'},
-    {type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut'}
+var users = [
+    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
+    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' }
 ];
-
-export type ApiResponse<T> = {status: 'success'; data: T} | {status: 'error'; error: string};
-
-export function requestAdmins(callback: (response: ApiResponse<Admin[]>) => void) {
+function requestAdmins(callback) {
     callback({
         status: 'success',
         data: admins
     });
 }
-
-export function requestUsers(callback: (response: ApiResponse<User[]>) => void) {
+exports.requestAdmins = requestAdmins;
+function requestUsers(callback) {
     callback({
         status: 'success',
         data: users
     });
 }
-
-export function requestCurrentServerTime(callback: (response: ApiResponse<number>) => void) {
+exports.requestUsers = requestUsers;
+function requestCurrentServerTime(callback) {
     callback({
         status: 'success',
         data: Date.now()
     });
 }
-
-export function requestCoffeeMachineQueueLength(callback: (response: ApiResponse<number>) => void) {
+exports.requestCurrentServerTime = requestCurrentServerTime;
+function requestCoffeeMachineQueueLength(callback) {
     callback({
         status: 'error',
         error: 'Numeric value has exceeded Number.MAX_SAFE_INTEGER.'
     });
 }
-
-function logPerson(person: Person) {
-    console.log(` - ${person.name}, ${person.age}, ${person.type === 'admin' ? person.role : person.occupation}`);
+exports.requestCoffeeMachineQueueLength = requestCoffeeMachineQueueLength;
+function logPerson(person) {
+    console.log(" - ".concat(person.name, ", ").concat(person.age, ", ").concat(person.type === 'admin' ? person.role : person.occupation));
 }
-
-function startTheApp(callback: (error: Error | null) => void) {
-    requestAdmins((adminsResponse) => {
+function startTheApp(callback) {
+    requestAdmins(function (adminsResponse) {
         console.log('Admins:');
         if (adminsResponse.status === 'success') {
             adminsResponse.data.forEach(logPerson);
-        } else {
+        }
+        else {
             return callback(new Error(adminsResponse.error));
         }
-
         console.log();
-
-        requestUsers((usersResponse) => {
+        requestUsers(function (usersResponse) {
             console.log('Users:');
             if (usersResponse.status === 'success') {
                 usersResponse.data.forEach(logPerson);
-            } else {
+            }
+            else {
                 return callback(new Error(usersResponse.error));
             }
-
             console.log();
-
-            requestCurrentServerTime((serverTimeResponse) => {
+            requestCurrentServerTime(function (serverTimeResponse) {
                 console.log('Server time:');
                 if (serverTimeResponse.status === 'success') {
-                    console.log(`   ${new Date(serverTimeResponse.data).toLocaleString()}`);
-                } else {
+                    console.log("   ".concat(new Date(serverTimeResponse.data).toLocaleString()));
+                }
+                else {
                     return callback(new Error(serverTimeResponse.error));
                 }
-
                 console.log();
-
-                requestCoffeeMachineQueueLength((coffeeMachineQueueLengthResponse) => {
+                requestCoffeeMachineQueueLength(function (coffeeMachineQueueLengthResponse) {
                     console.log('Coffee machine queue length:');
                     if (coffeeMachineQueueLengthResponse.status === 'success') {
-                        console.log(`   ${coffeeMachineQueueLengthResponse.data}`);
-                    } else {
+                        console.log("   ".concat(coffeeMachineQueueLengthResponse.data));
+                    }
+                    else {
                         return callback(new Error(coffeeMachineQueueLengthResponse.error));
                     }
-
                     callback(null);
                 });
             });
         });
     });
 }
-
-startTheApp((e: Error | null) => {
+startTheApp(function (e) {
     console.log();
     if (e) {
-        console.log(`Error: "${e.message}", but it's fine, sometimes errors are inevitable.`);
-    } else {
+        console.log("Error: \"".concat(e.message, "\", but it's fine, sometimes errors are inevitable."));
+    }
+    else {
         console.log('Success!');
     }
 });
-
 // In case you are stuck:
 // https://www.typescriptlang.org/docs/handbook/2/generics.html
